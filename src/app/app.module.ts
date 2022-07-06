d85862f8ad48d8238b346f1dd5bc3aa5c415bbc7
Jorge Palacio Barcinilla <jorgepalacio1995@gmail.com>
2022-07-05 19:18:12 -0500

    Affterclass internacionalizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { ThemeInitializerProvider } from './theme/theme-initializer.provider';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -28,6 +37,7 @@ import { ThemeInitializerProvider } from './theme/theme-initializer.provider';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     MatToolbarModule,
@@ -38,6 +48,15 @@ import { ThemeInitializerProvider } from './theme/theme-initializer.provider';
     MatFormFieldModule,
     MatInputModule,
     MatTableModule,
+    MatMenuModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    })
   ],
   providers: [
     ThemeInitializerProvider
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Language } from 'src/app/models/i18n.model';
 import { ThemeMode } from 'src/app/theme/models/theme.enum';
 import { Theme } from 'src/app/theme/models/theme.model';
 import { ThemeService } from 'src/app/theme/services/theme.service';
@@ -9,15 +11,21 @@ import { ThemeService } from 'src/app/theme/services/theme.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  Language = Language;
   ThemeMode = ThemeMode
   currentTheme: Theme
   currentDate = new Date();
+  currentLanguage: Language = this.translateService.defaultLang as Language;
 
   @Output('toggleSidenavButton') toggleSidenavButtonOutput = new EventEmitter();
   @Input() openSidenav!: boolean
 
-  constructor(private themeService: ThemeService) {
+  constructor(private themeService: ThemeService, private translateService:TranslateService) {
     this.currentTheme = this.themeService.getCurrentTheme();
+    this.translateService.onLangChange.subscribe((language) => {
+      console.log(language)
+      this.currentLanguage = language.lang as Language
+    });
   }
 
   ngOnInit(): void {
@@ -29,5 +37,9 @@ export class NavbarComponent implements OnInit {
 
   changeThemeMode(themeMode: ThemeMode){
     this.themeService.changeThemeMode(themeMode == ThemeMode.DARK ? ThemeMode.LIGHT : ThemeMode.DARK, themeMode);
+  }
+
+  changeLanguage(language: Language){
+    this.translateService.use(language)
   }
 }
